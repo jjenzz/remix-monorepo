376e4e2619247ea632be3f7e1f74368762814206
Jenna Smith <jenna@growldesign.co.uk>
2021-01-24 20:24:01 +0000

    prettier

--- app/pm2.config.js
+++ app/pm2.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  apps: [
-    {
-      name: "Express",
-      script: "index.js",
-      watch: ["remix.config.js", "src"],
-      watch_options: {
-        followSymlinks: false,
-      },
-      env: {
-        NODE_ENV: "development",
-      },
-    },
-    {
-      name: "Remix",
-      script: "remix run",
-      ignore_watch: ["."],
-      env: {
-        NODE_ENV: "development",
-      },
-    },
-  ],
+	apps: [
+		{
+			name: 'Express',
+			script: 'index.js',
+			watch: ['remix.config.js', 'src'],
+			watch_options: {
+				followSymlinks: false,
+			},
+			env: {
+				NODE_ENV: 'development',
+			},
+		},
+		{
+			name: 'Remix',
+			script: 'remix run',
+			ignore_watch: ['.'],
+			env: {
+				NODE_ENV: 'development',
+			},
+		},
+	],
 };
--- app/src/entry-server.tsx
+++ app/src/entry-server.tsx
@@ -1,30 +1,26 @@
-import ReactDOMServer from "react-dom/server";
-import type { EntryContext } from "@remix-run/core";
-import Remix from "@remix-run/react/server";
+import ReactDOMServer from 'react-dom/server';
+import type { EntryContext } from '@remix-run/core';
+import Remix from '@remix-run/react/server';
 
-import App, { ErrorBoundary } from "./App";
+import App, { ErrorBoundary } from './App';
 
 export default function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
+	request: Request,
+	responseStatusCode: number,
+	responseHeaders: Headers,
+	remixContext: EntryContext,
 ) {
-  let markup = ReactDOMServer.renderToString(
-    <Remix
-      context={remixContext}
-      url={request.url}
-      ErrorBoundary={ErrorBoundary}
-    >
-      <App />
-    </Remix>
-  );
+	let markup = ReactDOMServer.renderToString(
+		<Remix context={remixContext} url={request.url} ErrorBoundary={ErrorBoundary}>
+			<App />
+		</Remix>,
+	);
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: {
-      ...Object.fromEntries(responseHeaders),
-      "Content-Type": "text/html",
-    },
-  });
+	return new Response('<!DOCTYPE html>' + markup, {
+		status: responseStatusCode,
+		headers: {
+			...Object.fromEntries(responseHeaders),
+			'Content-Type': 'text/html',
+		},
+	});
 }
